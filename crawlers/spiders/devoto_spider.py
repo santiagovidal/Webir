5bd617275944deea848f7925127a3752034dd547
Santiago Vidal <svidal91@hotmail.com>
2015-10-19 19:50:40 -0300

    lalala

--- crawlers/spiders/devoto_spider.py
+++ crawlers/spiders/devoto_spider.py
@@ -11,17 +11,17 @@ class devotoSpider(CrawlSpider):
 	allowed_domains = ["devoto.com.uy"]
 	start_urls = [
 		"http://www.devoto.com.uy/acategory.aspx?1725"
-        
 	]
     # Asi es la URL del webservice de las paginas
     # El 5 despues del 4 es el numero de pagina
     # url = "http://www.devoto.com.uy/acategoryonload.aspx?1234,es,4,5,255,,,-1"
 	
-	rules = (Rule(SgmlLinkExtractor(restrict_xpaths="//ul[@class='subcategorias-lista']/li/a"),follow= True),)
-	
-	# def parse(self, response):
-		# print "hola"
+	rules = (Rule(SgmlLinkExtractor(restrict_xpaths="//ul[@class='subcategorias-lista']/li/a"),
+                  follow= True,
+                  callback="parseCategory"),)
     
-	def parse2(self, response):
-		print "hola22222"
-    # def parseCategory(self, response):
+	def parseCategory(self,response):
+        if not (response.xpath("//ul[@class='subcategorias-lista']/li/a")):
+            print("encontre pagina con productos")
+        else:
+            print("encontre cat")
