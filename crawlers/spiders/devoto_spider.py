59f88a846bdae504833cad4998d073f10210055b
camila sanz <camisanz2206@hotmail.com>
2015-10-19 17:23:52 -0300

    cambios en devoto_spider

--- crawlers/spiders/devoto_spider.py
+++ crawlers/spiders/devoto_spider.py
@@ -2,19 +2,26 @@ import scrapy
 from scrapy.http import FormRequest
 import json
 from scrapy.selector import Selector
+from scrapy.spiders import Rule, CrawlSpider
 from crawlers.items import Producto
+from scrapy.linkextractors.sgml import SgmlLinkExtractor
 
-class tInglesaSpider(scrapy.Spider):
-    name = "devoto"
-    allowed_domains = ["devoto.com.uy"]
-    start_urls = [
+class devotoSpider(CrawlSpider):
+	name = "devoto"
+	allowed_domains = ["devoto.com.uy"]
+	start_urls = [
+		"http://www.devoto.com.uy/acategory.aspx?1725"
         
-    ]
+	]
     # Asi es la URL del webservice de las paginas
     # El 5 despues del 4 es el numero de pagina
-    url = "http://www.devoto.com.uy/acategoryonload.aspx?1234,es,4,5,255,,,-1"
-
-    def parse(self,response):
-
-        
-    def parseCategory(self, response):
+    # url = "http://www.devoto.com.uy/acategoryonload.aspx?1234,es,4,5,255,,,-1"
+	
+	rules = (Rule(SgmlLinkExtractor(restrict_xpaths="//ul[@class='subcategorias-lista']/li/a"),follow= True),)
+	
+	# def parse(self, response):
+		# print "hola"
+    
+	def parse2(self, response):
+		print "hola22222"
+    # def parseCategory(self, response):
