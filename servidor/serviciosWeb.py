66ba5b7fa1f2c139a3d385eb6fa8e14915b9a907
camila sanz <camisanz2206@hotmail.com>
2015-11-07 21:13:53 -0300

    pase a correr todo en localhost

--- servidor/serviciosWeb.py
+++ servidor/serviciosWeb.py
@@ -1,16 +1,21 @@
 from flask import Flask
-from flask import jsonify
+from flask import render_template
+import sys
+sys.path.append('../UI')
 import bdAPI
 import json
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder="../UI", static_folder="../UI/static")
 
+@app.route("/")
+def index():
+    return render_template('index.html',)
 
 ###########formato json###########
 #{  marca: nomMarca,
 #   unidad: magnitud + metrica,   
 #   packpor: x + packpor`}
-@app.route("/datosPorProducto/<producto>")
+@app.route("/datosPorProducto/<producto>", methods=['GET'])
 def datosPorProducto(producto):
     return json.dumps(bdAPI.getDatosPorProducto("tinglesa", producto))
  
@@ -19,5 +24,5 @@ def datosPorProducto(producto):
  
 
 if __name__ == "__main__":
-    # app.debug = True
+    app.debug = True
     app.run()
